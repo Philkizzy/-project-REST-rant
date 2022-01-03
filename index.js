629ac45df83ba293a04fd87657c42de017daa54e
PhilGroup <64746978+PhilGroup@users.noreply.github.com>
2022-01-02 22:52:32 -0500

    Adding default view for layout

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) =>{
 })
 
 app.get('*', (req, res) =>{
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).send('<h1>404 Page not found</h1>')
 })
 
 app.listen(process.env.PORT)
